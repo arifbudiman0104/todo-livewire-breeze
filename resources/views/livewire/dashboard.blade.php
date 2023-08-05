8e33352b25b3cb140975fe470209fd1b34de2eca
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-08-05 22:10:13 +0700

    wip

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -5,7 +5,7 @@
     <div class="mt-5 space-y-5">
         <div class="bg-gray-100 dark:bg-gray-700 rounded-lg p-4 flex flex-col justify-between">
             <div>
-                <h2 class="text-lg font-semibold text-gray-900 dark:text-white">Todo Completed</h2>
+                <h2 class="text-lg font-semibold text-gray-900 dark:text-white">Todo Complete</h2>
                 <p class="mt-2 text-xl text-indigo-600 dark:text-indigo-300">{{ $todosCompleted }}</p>
             </div>
         </div>
