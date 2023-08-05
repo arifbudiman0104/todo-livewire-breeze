19388c66c51db88a0370eed00dd828a8ddbac855
Arif Budiman Arrosyid <arifbudiman01041996@hotmail.com>
2023-08-06 01:59:26 +0700

    wip

--- routes/web.php
+++ routes/web.php
@@ -18,21 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/todo', function () {
-    return view('todo.index');
-})->middleware(['auth', 'verified'])->name('todo.index');
-Route::get('/todo/create', function () {
-    return view('todo.create');
-})->middleware(['auth', 'verified'])->name('todo.create');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::view('/todo', 'todo.index')->name('todo.index');
+    Route::view('/todo/create', 'todo.create')->name('todo.create');
 });
 
 require __DIR__.'/auth.php';
